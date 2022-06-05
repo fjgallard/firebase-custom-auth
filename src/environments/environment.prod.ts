c7c3cc8a10ce45e598ce87a58433e4b6651a5af3
Francis Gallardo <fjdgallardo@gmail.com>
2022-06-05 16:10:11 +0800

    first commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
+  firebase: {
+    projectId: 'kokebrew',
+    appId: '1:495392732397:web:244486de15158cb9f288e4',
+    databaseURL: 'https://kokebrew.firebaseio.com',
+    storageBucket: 'kokebrew.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyD03FbxjjRe5xFcoLj6k6N7Ky7N6isFZkU',
+    authDomain: 'kokebrew.firebaseapp.com',
+    messagingSenderId: '495392732397',
+    measurementId: 'G-DXZ1YZJ85G',
+  },
   production: true
 };
